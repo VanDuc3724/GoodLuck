deac771450c859ac3db15cd378a59da620708579
VanDuc3724 <168082585+VanDuc3724@users.noreply.github.com>
2025-06-19 21:45:35 +0700

    Implement DbContext and add Migrations folder

--- GoodLuck/Repositories/DBContext.cs
+++ GoodLuck/Repositories/DBContext.cs
@@ -1,1 +1,15 @@
-﻿
+using Microsoft.EntityFrameworkCore;
+using GoodLuck.Models;
+
+namespace GoodLuck.Repositories
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Letter> Letters => Set<Letter>();
+        public DbSet<Anniversary> Anniversaries => Set<Anniversary>();
+    }
+}
